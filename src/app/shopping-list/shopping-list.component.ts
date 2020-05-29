40aa1911d9e640ac439ef23bda7a38db43e35859
charboubmustapha <mustaph.charboub@icloud.com>
2020-05-29 21:17:39 +0200

    polishing

--- src/app/shopping-list/shopping-list.component.ts
+++ src/app/shopping-list/shopping-list.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {Ingredient} from "../shared/ingredient.model";
 
 @Component({
   selector: 'app-shopping-list',
@@ -6,8 +7,16 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./shopping-list.component.css']
 })
 export class ShoppingListComponent implements OnInit {
+  ingredients: Ingredient[] = [
+    new Ingredient('chicken', 12),
+    new Ingredient('Onions', 10),
+    new Ingredient('Tomatoes', 10),
+    new Ingredient('Lemon', 4),
 
-  constructor() { }
+  ];
+
+  constructor() {
+  }
 
   ngOnInit(): void {
   }
